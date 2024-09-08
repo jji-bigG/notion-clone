a0e4aa1e834d05a971911608857b1dc1a8eda7ae
gerald <jji.bigg@gmail.com>
2024-09-08 16:14:46 -0400

    the user settings dropdown for the actions items

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -1,7 +1,38 @@
-import React from "react";
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { useUser } from "@clerk/clerk-react";
+import { PlusCircle } from "lucide-react";
+import Image from "next/image";
 
 const DocumentsPage = () => {
-  return <div>DocumentsPage, Protected</div>;
+  const { user } = useUser();
+
+  return (
+    <div className="h-full flex flex-col items-center justify-center space-y-4">
+      <Image
+        src="/empty.png"
+        height="300"
+        width="300"
+        alt="Empty"
+        className="dark:hidden"
+      />
+      <Image
+        src="/empty-dark.png"
+        height="300"
+        width="300"
+        alt="Empty"
+        className="hidden dark:block"
+      />
+      <h2 className="text-lg font-medium">
+        Welcome to {user?.firstName}&apos;s documents page!
+      </h2>
+      <Button>
+        <PlusCircle className="w-4 h-4 mr-2" />
+        Create a document
+      </Button>
+    </div>
+  );
 };
 
 export default DocumentsPage;
--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -5,6 +5,7 @@ import { ChevronsLeft, MenuIcon } from "lucide-react";
 import React, { ElementRef, use, useEffect, useRef, useState } from "react";
 import { usePathname } from "next/navigation";
 import { cn } from "@/lib/utils";
+import UserItem from "./user-item";
 
 export const Navigation = () => {
   const pathname = usePathname();
@@ -116,7 +117,7 @@ export const Navigation = () => {
           <ChevronsLeft className="w-6 h-6" />
         </div>
         <div>
-          <p>Action Items</p>
+          <UserItem />
         </div>
         <div className="mt-4">
           <p>Documents</p>
