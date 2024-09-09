a74eba3c33e6a9be1cfa95567396cea298f67875
gerald <jji.bigg@gmail.com>
2024-09-08 20:48:17 -0400

    archive operation

--- convex/documents.ts
+++ convex/documents.ts
@@ -17,6 +17,57 @@ export const get = query({
   },
 });
 
+export const archive = mutation({
+  args: {
+    id: v.id("documents"),
+  },
+  handler: async (ctx, args) => {
+    const identity = await ctx.auth.getUserIdentity();
+    if (!identity) {
+      throw new Error("Unauthorized");
+    }
+
+    const userId = identity.subject;
+
+    const existingDoc = await ctx.db.get(args.id);
+    if (!existingDoc) {
+      throw new Error("Document not found");
+    }
+
+    if (existingDoc.userId !== userId) {
+      throw new Error("Unauthorized"); // we are logged in, but we just cannot modify it
+    }
+
+    // also need to check all of its children: recursive function
+    const recursiveArchive = async (docId: Id<"documents">) => {
+      const children = await ctx.db
+        .query("documents")
+        .withIndex("by_user_parent", (q) =>
+          q.eq("userId", userId).eq("parentDocument", docId)
+        )
+        .collect();
+
+      // iterate through all the children and repeat the archive process
+      for (const child of children) {
+        // only way to do the async await function: need this for loop
+        await ctx.db.patch(child._id, {
+          isArchived: true,
+        });
+        await recursiveArchive(child._id);
+      }
+
+      await ctx.db.patch(docId, {
+        isArchived: true,
+      });
+    };
+
+    const doc = await ctx.db.patch(args.id, {
+      isArchived: true,
+    });
+    return doc;
+  },
+});
+
 export const getSidebar = query({
   args: {
     parentDocument: v.optional(v.id("documents")),
