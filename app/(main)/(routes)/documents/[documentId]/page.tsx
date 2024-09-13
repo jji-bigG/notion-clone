08d7cc3408c67f27d519455f43a4045be4521835
gerald <jji.bigg@gmail.com>
2024-09-13 16:49:20 -0400

    emoji picker when hover the title. real time title updates on the main thing

--- app/(main)/(routes)/documents/[documentId]/page.tsx
+++ app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -1,7 +1,40 @@
+"use client";
+
+import { Toolbar } from "@/components/toolbar";
+import { api } from "@/convex/_generated/api";
+import { Id } from "@/convex/_generated/dataModel";
+import { useQuery } from "convex/react";
 import React from "react";
 
-const DocumentIdPage = () => {
-  return <div>DocumentIdPage</div>;
+interface DocumentIdPageProps {
+  params: {
+    documentId: Id<"documents">;
+  };
+}
+
+const DocumentIdPage = ({ params }: DocumentIdPageProps) => {
+  const document = useQuery(api.documents.getById, {
+    id: params.documentId,
+  });
+
+  if (document === undefined) {
+    return <p>Loading...</p>;
+  }
+
+  if (document === null) {
+    return <p>Document not found</p>;
+  }
+
+  return (
+    <div className="pb-40">
+      {/* below is a temp placeholder (i don't wish to delete) for the cover image */}
+      {/* a trick that we can do we create some fillers for now dev'ing the toolbars */}
+      <div className="h-[35vh]" />
+      <div className="md:max-w-3xl lg:max-w-4xl mx-auto">
+        <Toolbar initialData={document} />
+      </div>
+    </div>
+  );
 };
 
 export default DocumentIdPage;
