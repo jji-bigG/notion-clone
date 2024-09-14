972d674f87dffead324a639e758eb80a2a146c45
gerald <jji.bigg@gmail.com>
2024-09-13 20:23:31 -0400

    checkpoint: stashing for 6:41:55 in the tutorial. working on image cover

--- app/(main)/(routes)/documents/[documentId]/page.tsx
+++ app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { CoverImage } from "@/components/cover";
 import { Toolbar } from "@/components/toolbar";
 import { api } from "@/convex/_generated/api";
 import { Id } from "@/convex/_generated/dataModel";
@@ -27,9 +28,11 @@ const DocumentIdPage = ({ params }: DocumentIdPageProps) => {
 
   return (
     <div className="pb-40">
+      <CoverImage />
+
       {/* below is a temp placeholder (i don't wish to delete) for the cover image */}
       {/* a trick that we can do we create some fillers for now dev'ing the toolbars */}
-      <div className="h-[35vh]" />
+      {/* <div className="h-[35vh]" /> */}
       <div className="md:max-w-3xl lg:max-w-4xl mx-auto">
         <Toolbar initialData={document} />
       </div>
---
+++ components/cover.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+interface CoverImageProps {
+  url?: string;
+  preview?: boolean;
+}
+
+export const CoverImage = ({ url, preview }: CoverImageProps) => {
+  return <div>Cover image</div>;
+};
