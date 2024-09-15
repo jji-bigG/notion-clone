ba403a5a3e36d5ab3bad711ca51854eb079abb1a
gerald <jji.bigg@gmail.com>
2024-09-15 00:38:52 -0400

    cover image can be added

--- components/cover.tsx
+++ components/cover.tsx
@@ -4,6 +4,12 @@ import { cn } from "@/lib/utils";
 import Image from "next/image";
 import { Button } from "./ui/button";
 import { ImageIcon, X } from "lucide-react";
+import { useCoverImage } from "@/hooks/use-cover-image";
+import { useMutation } from "convex/react";
+import { api } from "@/convex/_generated/api";
+import { useParams } from "next/navigation";
+import { Id } from "@/convex/_generated/dataModel";
+import { useEdgeStore } from "@/lib/edgestore";
 
 interface CoverImageProps {
   url?: string;
@@ -11,6 +17,19 @@ interface CoverImageProps {
 }
 
 export const CoverImage = ({ url, preview }: CoverImageProps) => {
+  const { edgestore } = useEdgeStore();
+
+  const params = useParams();
+  const coverImage = useCoverImage();
+  const removeCoverImage = useMutation(api.documents.removeCoverImage);
+
+  const onRemove = async () => {
+    if (url) {
+      edgestore.publicFiles.delete({ url });
+    }
+    removeCoverImage({ id: params.documentId as Id<"documents"> });
+  };
+
   return (
     <div
       className={cn(
@@ -22,12 +41,13 @@ export const CoverImage = ({ url, preview }: CoverImageProps) => {
       {!!url && (
         <Image src={url} fill alt="Cover Image" className="object-cover" />
       )}
-      {url && (
+      {url && !preview && (
         <div className="opacity-0 group-hover:opacity-100 absolute bottom-5 right-5 flex items-center gap-x-2">
           <Button
             className="text-muted-foreground text-xs"
             variant="outline"
             size="sm"
+            onClick={() => coverImage.onReplace(url)}
           >
             <ImageIcon className="h-4 w-4 mr-2" onClick={() => {}} />
             Change Cover
@@ -36,6 +56,7 @@ export const CoverImage = ({ url, preview }: CoverImageProps) => {
             className="text-muted-foreground text-xs"
             variant="outline"
             size="sm"
+            onClick={onRemove}
           >
             <X className="h-4 w-4 mr-2" onClick={() => {}} />
             Remove
